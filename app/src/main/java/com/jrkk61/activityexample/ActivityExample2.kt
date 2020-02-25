decd52ff22bf05e563a9cc05923c5bc40338e827
Jayna-Menard <31700864+JaynaMMenard@users.noreply.github.com>
2020-02-25 16:27:17 -0500

    Undoing Changes

---
+++ app/src/main/java/com/jrkk61/activityexample/ActivityExample2.kt
@@ -0,0 +1,12 @@
+package com.jrkk61.activityexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityExample2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example2)
+    }
+}
