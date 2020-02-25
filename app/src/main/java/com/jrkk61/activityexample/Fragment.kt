4be8f953219ae2af5aa37c3055d77c08b68e830a
JaynaMMenard <jaynamenard@gmail.com>
2020-02-25 13:54:47 -0500

    Fragment XML Modified

--- app/src/main/java/com/jrkk61/activityexample/Fragment.kt
+++ app/src/main/java/com/jrkk61/activityexample/Fragment.kt
@@ -1,10 +1,21 @@
 package com.jrkk61.activityexample
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 
 
 class Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return LayoutInflater.from(container?.context).inflate(R.layout.fragment_,container,false)
+    }
 
 }
