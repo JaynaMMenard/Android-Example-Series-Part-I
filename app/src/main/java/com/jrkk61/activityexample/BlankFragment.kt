a72d7665d89b630e2c28b31762da8afa1be7ed3a
JaynaMMenard <jaynamenard@gmail.com>
2020-02-25 23:17:52 -0500

    Fragments Files Modified

--- app/src/main/java/com/jrkk61/activityexample/BlankFragment.kt
+++ app/src/main/java/com/jrkk61/activityexample/BlankFragment.kt
@@ -27,11 +27,4 @@ class BlankFragment : Fragment() {
     }
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //you can set the title for your toolbar here for different fragments different titles
-        activity!!.title = "Fragment1"
-    }
-
-
 }
--- app/src/main/java/com/jrkk61/activityexample/BlankFragment2.kt
+++ app/src/main/java/com/jrkk61/activityexample/BlankFragment2.kt
@@ -17,11 +17,7 @@ class BlankFragment2 : Fragment() {
     }
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //you can set the title for your toolbar here for different fragments different titles
-        activity!!.title = "Fragment2"
-    }
+
 
 
 }
